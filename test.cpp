c1cee90ddf921b196796ab46f09b6c57c42d2149
Jitu Kumar Jha <jhajitu2002@gmail.com>
2022-08-24 12:22:42 +0530

    palindrome of an  int

--- test.cpp
+++ test.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int palindrome(int n,int b){
-    int s=0;
-     do
-     {
-      int a=n%10;
-      int s=s*10+a;
-      n=n/10;
-       if(s==b)
-         {
-           bool x=1;
-          cout<<x;
-            break;
-         }
-     }while(n!=0);
-    if(s!=b){
-        bool x=0;
-        cout<<x;
+int palindrome(int n)
+{
+    int s = 0;
+    int b = n;
+    for (int i = 0; i < n; i++)
+    {
+        int a = n % 10;
+        s = s * 10 + a;
+        n = n / 10;
+        if (n == 0)
+        {
+            if (s == b)
+            {
+                return 1;
+            }
+        }
+        if (s != b)
+        {
+            return 0;
+        }
     }
-    return 0;
 }
 int main()
-{   int n;
-cin>>n;
-int b=n;
-  palindrome(n,b);
-return 0;
+{
+    int n;
+    cin >> n;
+    palindrome(n);
+    return 0;
 }
