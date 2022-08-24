84ce77e794cac4c4a96af0b512968bf6fd294952
JHAGOVIND <65236338+JHAGOVIND@users.noreply.github.com>
2022-08-24 14:03:23 +0530

    Update prime.cpp
    updated review changes

--- prime.cpp
+++ prime.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 bool prime(int n){
     if(n<=1){
         return false;
     }
     
-    for(int i=2;i<n;i++){
+    for(int i=2;i<sqrt(n);i++){
         if(n%i==0){
             return false;
         }
