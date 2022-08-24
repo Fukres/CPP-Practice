eb79b84841ccafc22a63a0c3e006d30afc374cb4
Govind Kumar Jha <govindkumarjha2017@gmail.com>
2022-08-24 12:22:49 +0530

    review comment resolve

--- prime.cpp
+++ prime.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
 using namespace std;
 bool prime(int n){
-    if(n==1){
+    if(n<=1){
         return false;
     }
-    else if(n==2){
-        return true;
-    }
-    else if(n>2){
+    
     for(int i=2;i<n;i++){
         if(n%i==0){
             return false;
         }
     }
+
     return true;
-    }
+
 }
 int main(){
     int n;
