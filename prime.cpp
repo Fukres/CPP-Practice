0d809c4f9a19ca8ec64de112f5f26f9dac904946
Govind Kumar Jha <govindkumarjha2017@gmail.com>
2022-08-24 12:15:22 +0530

    prime number code

--- prime.cpp
+++ prime.cpp
@@ -4,19 +4,16 @@ bool prime(int n){
     if(n==1){
         return false;
     }
-    if(n==2){
+    else if(n==2){
         return true;
     }
-    int temp=0;
+    else if(n>2){
     for(int i=2;i<n;i++){
         if(n%i==0){
-            temp=1;
+            return false;
         }
     }
-    if(temp==0)
     return true;
-    if(temp==1){
-     return false;
     }
 }
 int main(){
