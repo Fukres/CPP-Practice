cade6a0bdaef2c234b6b3113aa49814b6fc39de9
Jitu Kumar Jha <jhajitu2002@gmail.com>
2022-08-24 10:56:14 +0530

    palindrome of intiger no.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
+int palindrome(int n,int b){
+    int s=0;
+     do
+     {
+      int a=n%10;
+      int s=s*10+a;
+      n=n/10;
+       if(s==b)
+         {
+           bool x=1;
+          cout<<x;
+            break;
+         }
+     }while(n!=0);
+    if(s!=b){
+        bool x=0;
+        cout<<x;
+    }
+    return 0;
+}
 int main()
-{
-    cout << "Hello World";
+{   int n;
+cin>>n;
+int b=n;
+  palindrome(n,b);
+return 0;
 }
